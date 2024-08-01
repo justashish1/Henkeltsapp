0c4867d358797b359efacd69c3c9860428cd54b1
Ashish Malviya <60316270+justashish1@users.noreply.github.com>
2024-08-01 15:35:57 +0530

    Update app.py with ver.1.0.21

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def custom_css():
                 font-size: 12px;
                 font-weight: bold;
                 position: fixed;
-                bottom: 10px;
+                bottom: 50px;
                 right: 10px;
                 background-color: #FF0000;
                 color: white !important;
